e2c007a46a57b18d5a81adbbfdb06a47af7d15ad
fgbyte <fer.garciabayon@gmail.com>
2025-06-18 12:48:55 -0400

    Fix: Update SQLite URL and add auth router

--- fastapi/api/database.py
+++ fastapi/api/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQL_ALCHEMY_DATABASE_URL = 'sqlite://workout_app.db'
+SQL_ALCHEMY_DATABASE_URL = 'sqlite:///workout_app.db'
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args={
                        'check_same_thread': False})
